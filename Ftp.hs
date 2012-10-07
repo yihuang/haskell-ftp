7706922bcfd0a7f25e69acc995988ef0c1aa2b3d
yihuang <yi.codeplayer@gmail.com>
2012-10-07 16:36:57 +0800

    Use settings type.

--- Ftp.hs
+++ Ftp.hs
@@ -1,13 +1,13 @@
 {-# LANGUAGE ViewPatterns #-}
 import System.Environment (getArgs)
 import Filesystem.Path.CurrentOS (decodeString)
-import Data.Conduit.Network (runTCPServer, serverSettings, HostPreference(HostAny))
+import Data.Conduit.Network (runTCPServer, serverSettings, HostPreference(HostAny), serverNeedLocalAddr)
 import Network.FTP.Server (ftpServer)
 import Network.FTP.Backend.FileSystem (runFSBackend, FSConf(FSConf))
 
 main :: IO ()
 main = do
     [read -> port, d] <- getArgs
-    let serverConf = serverSettings port HostAny True
+    let serverConf = (serverSettings port HostAny){serverNeedLocalAddr=True}
     runFSBackend (FSConf (decodeString d)) $
         runTCPServer serverConf ftpServer
